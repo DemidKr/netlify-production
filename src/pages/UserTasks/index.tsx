64c3bb1ffe368fb1360a7a5621f416c6644182fd
Kirill <kirill-garnov@mail.ru>
2023-10-13 18:28:36 +0300

    added margin for user

--- src/pages/UserTasks/index.tsx
+++ src/pages/UserTasks/index.tsx
@@ -1,19 +1,19 @@
-import { useEffect, useState, useRef } from "react";
-import { v4 as v4uuid } from "uuid";
+import { useEffect, useState, useRef } from 'react';
+import { v4 as v4uuid } from 'uuid';
 
-import { Columns } from "../../components/Columns";
-import { Box } from "@mui/material";
-import { PRIORITY_ENUM, ColumnType, TaskType } from "../../entities";
-import { MainWrapper } from "../../components";
-import api from "../../utils/axios";
-import { AxiosResponse } from "axios";
+import { Columns } from '../../components/Columns';
+import { Box } from '@mui/material';
+import { PRIORITY_ENUM, ColumnType, TaskType, ISprint } from '../../entities';
+import { MainWrapper } from '../../components';
+import api from '../../utils/axios';
+import { AxiosResponse } from 'axios';
 
 const EMPTY_TASK = {
-  name: "",
-  description: "",
+  name: '',
+  description: '',
   status: PRIORITY_ENUM.LOW,
-  author: { id: "12", first_name: "Oleg", second_name: "Petrov" },
-  executor: { id: "23", first_name: "Ryan", second_name: "Gosling" },
+  author: { id: '12', first_name: 'Oleg', second_name: 'Petrov' },
+  executor: { id: '23', first_name: 'Ryan', second_name: 'Gosling' },
   hidden: true,
 };
 
@@ -21,182 +21,126 @@ export const UserTasks = () => {
   const requested = useRef(false);
   const [columns, setColumns] = useState<ColumnType[]>([
     {
-      id: "1",
-      name: "Хранилище",
+      id: '1',
+      name: 'Хранилище',
       tasks: [
         {
           ...EMPTY_TASK,
           id: v4uuid(),
         },
       ],
-      color: "#1E293B",
-      showMenu: true,
-    },
-    {
-      id: "2",
-      name: "В работу",
-      showMenu: true,
-      tasks: [
-        {
-          name: "1234",
-          description:
-            "Brainstorming brings team members' diverse experience into play.",
-          status: PRIORITY_ENUM.LOW,
-          author: { id: "12", first_name: "Oleg", second_name: "Petrov" },
-          executor: { id: "23", first_name: "Ryan", second_name: "Gosling" },
-          id: v4uuid(),
-          hidden: true,
-        },
-        {
-          name: "Onboarding Illustrations ",
-          description:
-            "Low fidelity wireframes include the most basic content and visuals.",
-          status: PRIORITY_ENUM.HIGH,
-          author: { id: "12", first_name: "Remy", second_name: "Sharp" },
-          executor: {
-            id: "23",
-            first_name: "Travis",
-            second_name: "Howard",
-          },
-          id: "14",
-        },
-        {
-          name: "222",
-          description:
-            "Low fidelity wireframes include the most basic content and visuals.",
-          status: PRIORITY_ENUM.MEDIUM,
-          author: { id: "12", first_name: "Cindy", second_name: "Baker" },
-          executor: {
-            id: "23",
-            first_name: "Travis",
-            second_name: "Howard",
-          },
-          id: "11231235",
-        },
-      ],
-      color: "#5030E5",
-    },
-    {
-      id: "3",
-      name: "В работе",
+      color: '#1E293B',
       showMenu: false,
-      tasks: [
-        {
-          name: "1234",
-          description:
-            "Brainstorming brings team members' diverse experience into play.",
-          status: PRIORITY_ENUM.LOW,
-          author: { id: "12", first_name: "Oleg", second_name: "Petrov" },
-          executor: { id: "23", first_name: "Ryan", second_name: "Gosling" },
-          id: v4uuid(),
-          hidden: true,
-        },
-        {
-          name: "Brainstorming",
-          description:
-            "Low fidelity wireframes include the most basic content and visuals.",
-          status: PRIORITY_ENUM.LOW,
-          author: { id: "12", first_name: "Dmitry", second_name: "Olegov" },
-          executor: {
-            id: "23",
-            first_name: "Travis",
-            second_name: "Howard",
-          },
-          id: "16",
-        },
-        {
-          name: "Mobile App Design",
-          description:
-            "Low fidelity wireframes include the most basic content and visuals.",
-          status: PRIORITY_ENUM.HOT,
-          author: { id: "12", first_name: "Dmitry", second_name: "Olegov" },
-          executor: { id: "12", first_name: "Cindy", second_name: "Baker" },
-          id: "17",
-        },
-      ],
-      color: "#FFA500",
     },
     {
-      id: "4",
-      name: "Тестирование",
-      showMenu: false,
+      id: '2',
+      name: 'В работу',
       tasks: [
         {
-          name: "1234",
-          description:
-            "Brainstorming brings team members' diverse experience into play.",
-          status: PRIORITY_ENUM.LOW,
-          author: { id: "12", first_name: "Oleg", second_name: "Petrov" },
-          executor: { id: "23", first_name: "Ryan", second_name: "Gosling" },
+          ...EMPTY_TASK,
           id: v4uuid(),
-          hidden: true,
-        },
-        {
-          name: "1234",
-          description:
-            "Low fidelity wireframes include the most basic content and visuals.",
-          status: PRIORITY_ENUM.HIGH,
-          author: { id: "12", first_name: "Dmitry", second_name: "Olegov" },
-          executor: { id: "12", first_name: "Cindy", second_name: "Baker" },
-          id: "18",
-        },
-        {
-          name: "2225",
-          description:
-            "Low fidelity wireframes include the most basic content and visuals.",
-          status: PRIORITY_ENUM.MEDIUM,
-          author: { id: "12", first_name: "Cindy", second_name: "Baker" },
-          executor: { id: "12", first_name: "Remy", second_name: "Sharp" },
-          id: "19",
         },
       ],
-      color: "#33BFFF",
+      color: '#5030E5',
+      showMenu: false,
     },
     {
-      id: "5",
-      name: "Готово",
-      showMenu: false,
+      id: '3',
+      name: 'В работе',
       tasks: [
         {
-          name: "1234",
-          description:
-            "Brainstorming brings team members' diverse experience into play.",
-          status: PRIORITY_ENUM.LOW,
-          author: { id: "12", first_name: "Oleg", second_name: "Petrov" },
-          executor: { id: "23", first_name: "Ryan", second_name: "Gosling" },
+          ...EMPTY_TASK,
           id: v4uuid(),
-          hidden: true,
-        },
-        {
-          name: "1234",
-          description:
-            "Low fidelity wireframes include the most basic content and visuals.",
-          status: PRIORITY_ENUM.LOW,
-          author: { id: "12", first_name: "Dmitry", second_name: "Olegov" },
-          executor: { id: "23", first_name: "Ryan", second_name: "Gosling" },
-          id: "20",
-        },
-        {
-          name: "Design System",
-          description:
-            "Low fidelity wireframes include the most basic content and visuals.",
-          status: PRIORITY_ENUM.HOT,
-          author: { id: "12", first_name: "Dmitry", second_name: "Olegov" },
-          executor: { id: "23", first_name: "Ryan", second_name: "Gosling" },
-          id: "21",
         },
       ],
-      color: "#8BC48A",
+      color: '#FFA500',
+      showMenu: false,
+    },
+    {
+      id: '4',
+      name: 'Тестирование',
+      tasks: [
+        {
+          ...EMPTY_TASK,
+          id: v4uuid(),
+        },
+      ],
+      color: '#33BFFF',
+      showMenu: false,
+    },
+    {
+      id: '5',
+      name: 'Готово',
+      tasks: [
+        {
+          ...EMPTY_TASK,
+          id: v4uuid(),
+        },
+      ],
+      color: '#8BC48A',
+      showMenu: false,
     },
   ]);
 
   useEffect(() => {
     if (!requested.current) {
       api()
-        .get("/task/all")
+        .get('/sprint/all')
+        .then((body: AxiosResponse<ISprint[]>) => {
+          if (body?.data?.length > 0) {
+            //получили текущий спринт
+            api()
+              .get(`/sprint/${body?.data?.[0]?.id}/tasks`)
+              .then((body: AxiosResponse<TaskType[]>) => {
+                if (body?.data) {
+                  //получили задачи текущего спринта
+                  //сетим для туду столбца все новые задачи
+                  setColumns(prevColumns => {
+                    const newColumns = JSON.parse(JSON.stringify(prevColumns));
+
+                    newColumns[1].tasks = [
+                      {
+                        ...EMPTY_TASK,
+                        id: v4uuid(),
+                      },
+                      ...body?.data.filter(task => task.status_id === 2),
+                    ];
+
+                    newColumns[2].tasks = [
+                      {
+                        ...EMPTY_TASK,
+                        id: v4uuid(),
+                      },
+                      ...body?.data.filter(task => task.status_id === 3),
+                    ];
+
+                    newColumns[3].tasks = [
+                      {
+                        ...EMPTY_TASK,
+                        id: v4uuid(),
+                      },
+                      ...body?.data.filter(task => task.status_id === 4),
+                    ];
+
+                    newColumns[4].tasks = [
+                      {
+                        ...EMPTY_TASK,
+                        id: v4uuid(),
+                      },
+                      ...body?.data.filter(task => task.status_id === 5),
+                    ];
+
+                    return newColumns;
+                  });
+                }
+              });
+          }
+        });
+      api()
+        .get('/task/all')
         .then((body: AxiosResponse<TaskType[]>) => {
           if (body?.data) {
-            setColumns((prevColumns) => {
+            setColumns(prevColumns => {
               const newColumns = JSON.parse(JSON.stringify(prevColumns));
               newColumns[0].tasks = [
                 {
@@ -217,7 +161,7 @@ export const UserTasks = () => {
 
   return (
     <MainWrapper>
-      <Box>
+      <Box mt="28px">
         <Columns columns={columns} setColumns={setColumns} />
       </Box>
     </MainWrapper>
